268d2ea0ad81166a99130f1c49c91f9cfb269153
Re-taro <aminokacho@gmail.com>
2021-12-04 20:28:23 +0900

    ♻️ Refactor to native qr reader
    it is test deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState, useRef } from 'react'
+import { FC, useEffect, useState, useRef } from 'react'
 import { BrowserQRCodeReader, IScannerControls } from '@zxing/browser'
 import { Result } from '@zxing/library'
 import {
@@ -13,6 +13,7 @@ import {
   Td,
   Tr
 } from '@chakra-ui/react'
+import liff from '@line/liff/dist/lib'
 
 const QRCodeReader: FC<{ onReadQRCode: (text: Result) => void }> = ({ onReadQRCode }) => {
   const controlRef = useRef<IScannerControls | null>()
@@ -48,7 +49,67 @@ const QRCodeReader: FC<{ onReadQRCode: (text: Result) => void }> = ({ onReadQRCo
   return (
     <video
       style={{ maxWidth: "100%", maxHeight: "100%", height: "100%" }}
-      ref={videoRef}
+      ref={ videoRef }
     />
   )
 }
+
+/**This is for debugging **/
+const QRCodeResult: FC<{ QRCodes: string[] }> = ({ QRCodes }) => {
+  return (
+    <Table>
+      <Tbody>
+        {QRCodes.map((QR, i) => (
+          <Tr key={i}>
+            <Td>
+              <Fade in={true}>{QR}</Fade>
+            </Td>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
+  )
+}
+
+const App: FC = () => {
+  const [QRCodes, setQRCodes] = useState<string[]>([])
+  return (
+    <ChakraProvider>
+      <Container>
+        <Flex flexDirection='column'>
+          <Box flex={1} height={'60vh'}>
+            <QRCodeReader
+              onReadQRCode={(result) => {
+                liff.init({ liffId: process.env.LIFF_ID as string || process.env.REACT_APP_LIFF_ID as string })
+                  .then(() => {
+                    if (!liff.isLoggedIn()) {
+                      liff.login()
+                    }
+                    liff.getProfile()
+                      .then((profile) => {
+                        const data = `${result.getText()}, ${profile.userId}`
+                        setQRCodes((codes) => {
+                          return [data, ...codes]
+                        })
+                      })
+                      .catch((e: unknown) => {
+                        console.error(e)
+                      })
+                  })
+                  .catch((e: unknown) => {
+                    console.error(e)
+                  })
+              }}
+            />
+          </Box>
+          <Box flex={1} height={'40vh'}>
+            <Heading>Result</Heading>
+            <QRCodeResult QRCodes={QRCodes} />
+          </Box>
+        </Flex>
+      </Container>
+    </ChakraProvider>
+  )
+}
+
+export default App
