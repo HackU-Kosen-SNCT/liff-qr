83aa1e214b9fa1be3a945b2724d2de4fdb3f85ae
Re-taro <aminokacho@gmail.com>
2021-12-04 16:21:10 +0900

    ✨ Update App.tsx
    Add button to open qr reader

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,57 @@
 import React from 'react'
-import logo from './logo.svg';
 import liff from '@line/liff'
+import Button from '@material-ui/core/Button';
 import './App.css';
 
-const qr = ():void => {
-  liff.init({liffId: process.env.LIFF_ID as string || process.env.REACT_APP_LIFF_ID as string})
-    .then(() => {
-      if(!liff.isLoggedIn()) {
-        liff.login({})
-      }
-      else if(liff.isInClient()) {
-        liff.scanCodeV2()
-          .then((result) => {
-            const itemId = result.value
-            if ( itemId ) {
-              liff.getProfile()
-                .then((profile) => {
-                  const userId = profile.userId
-                  console.log(itemId, userId) // TODO post to server
-                })
-                .catch((e: unknown) => {
-                  console.error(e)
-                })
-            }
-            else {
-              console.error('Invalid itemId is null or out of range.')
-            }
-          })
-          .then(() => {
-            liff.closeWindow()
-          })
-          .catch((e: unknown) => {
-            console.error(e)
-          })
-      }
-      else {
-        console.error('Login process did not complete')
-      }
-    })
-    .catch((e: unknown) => {
-      console.error(e)
-    })
-}
-
 const App: React.FC = () => {
-  React.useEffect(() => {
-    qr()
-  }, [])
+  const [ value, setValue ] = React.useState<string>('')
+  const qr = ():void => {
+    liff.init({liffId: process.env.LIFF_ID as string || process.env.REACT_APP_LIFF_ID as string})
+      .then(() => {
+        if(!liff.isLoggedIn()) {
+          liff.login({})
+        }
+        else if(liff.isInClient()) {
+          liff.scanCodeV2()
+            .then((result) => {
+              const itemId = result.value
+              if ( itemId ) {
+                liff.getProfile()
+                  .then((profile) => {
+                    const userId = profile.userId
+                    setValue(`${itemId}, ${userId}`) // TODO post to server
+                  })
+                  .catch((e: unknown) => {
+                    console.error(e)
+                  })
+              }
+              else {
+                console.error('Invalid itemId is null or out of range.')
+              }
+            })
+            .then(() => {
+              liff.closeWindow()
+            })
+            .catch((e: unknown) => {
+              console.error(e)
+            })
+        }
+        else {
+          console.error('Login process did not complete')
+        }
+      })
+      .catch((e: unknown) => {
+        console.error(e)
+      })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="qrValue">{value}</div>
+      <div className="qrButton">
+        <Button variant="contained" color="primary" onClick={() => qr()}>
+          Open QR camera
+        </Button>
+      </div>
     </div>
   );
 }
