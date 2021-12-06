b6039adf88e0397206a30b3a8d24c42151c4dc68
Re-taro <aminokacho@gmail.com>
2021-12-07 00:36:56 +0900

    ✨ Add vconsole

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,8 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import QR from './components/pages/QR'
 import Result from './components/pages/Result'
-import VConsole from 'vconsole'
 
 const App: React.FC = () => {
-  const vConsole = new VConsole()
   return (
     <Router>
       <Routes>
