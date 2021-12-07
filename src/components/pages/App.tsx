77f4959bc19710b3477bd2dc2c94d92c0f993f4b
Re-taro <aminokacho@gmail.com>
2021-12-08 00:16:20 +0900

    ✨ Add login process

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import QR from './QR'
 import Result from './Result'
+import liff from '@line/liff'
 
 const App: React.FC = () => {
+  useEffect(() => {
+    liff.init({ liffId: process.env.REACT_APP_LIFF_ID as string })
+      .then(() => {
+        if (!liff.isLoggedIn()) {
+          liff.login()
+        }
+      })
+  }, [])
   return (
     <Router>
       <Routes>
