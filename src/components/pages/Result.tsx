5a012dfd4136ed297d368ada41d2fb489e507a65
Re-taro <aminokacho@gmail.com>
2021-12-13 00:30:52 +0900

    🐛 Fix style

--- src/components/pages/Result.tsx
+++ src/components/pages/Result.tsx
@@ -14,10 +14,10 @@ const Result: FC = () => {
     <ChakraProvider>
       <Container>
         <Flex flexDirection='column' alignItems='center'>
-          <Box>
+          <Box alignItems='center'>
             <Text color="#02331b" align="center">登録が完了しました！</Text>
             <Text color="#5a7165" align="center">ブラウザを閉じてください。</Text>
-            <Image alt="Thank you!" src={thankyou} w="50%" h="50%" align="center" />
+            <Image alt="Thank you!" src={thankyou} w="50%" h="50%" />
           </Box>
         </Flex>
       </Container>
