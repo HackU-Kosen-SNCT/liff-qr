06f1b38a1fb39b87847e7a44fe37b7ec24f5f532
Re-taro <aminokacho@gmail.com>
2021-12-13 00:08:50 +0900

    🎨 Chakra UI is GOD

--- src/components/pages/Result.tsx
+++ src/components/pages/Result.tsx
@@ -4,14 +4,10 @@ import {
   Container,
   Flex,
   Box,
-  Text
+  Text,
+  Image
 } from '@chakra-ui/react'
 import thankyou from '../../assets/thankyou.svg'
-import styled from 'styled-components'
-
-const StyledDiv = styled.div`
-  text-align: center
-`
 
 const Result: FC = () => {
   return (
@@ -21,9 +17,7 @@ const Result: FC = () => {
           <Box>
             <Text color="#02331b" align="center">登録が完了しました！</Text>
             <Text color="#5a7165" align="center">ブラウザを閉じてください。</Text>
-            <StyledDiv>
-              <img alt="Thank you!" src={thankyou} width="50%" height="50%" />
-            </StyledDiv>
+            <Image alt="Thank you!" src={thankyou} w="50%" h="50%" align="center" />
           </Box>
         </Flex>
       </Container>
