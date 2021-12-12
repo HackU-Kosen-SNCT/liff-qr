ba55f848fbffecf0befe40dd28268c1082d90036
Re-taro <aminokacho@gmail.com>
2021-12-12 22:25:12 +0900

    🚧 Change SVG size

--- src/components/pages/Result.tsx
+++ src/components/pages/Result.tsx
@@ -16,7 +16,7 @@ const Result: FC = () => {
           <Box>
             <Text color="#02331b" align="center">登録が完了しました！</Text>
             <Text color="#5a7165" align="center">ブラウザを閉じてください。</Text>
-            <img alt="Thank you!" src={thankyou} />
+            <img alt="Thank you!" src={thankyou} width="80%" height="80%" />
           </Box>
         </Flex>
       </Container>
