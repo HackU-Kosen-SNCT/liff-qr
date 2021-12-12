3ff9fe07b81efc9052ff6ad4fee8b372fabd5e01
Re-taro <aminokacho@gmail.com>
2021-12-12 21:39:46 +0900

    🚧 Refactor SVG

--- src/components/pages/Result.tsx
+++ src/components/pages/Result.tsx
@@ -6,7 +6,7 @@ import {
   Box,
   Text
 } from '@chakra-ui/react'
-import { ReactComponent as Icon } from '../../assets/thankyou.svg'
+import thankyou from '../../assets/thankyou.svg'
 
 const Result: FC = () => {
   return (
@@ -16,7 +16,7 @@ const Result: FC = () => {
           <Box>
             <Text color="#02331b" align="center">登録が完了しました！</Text>
             <Text color="#5a7165" align="center">ブラウザを閉じてください。</Text>
-            <Icon />
+            <img alt="Thank you!" src={thankyou} width="10vh" height="10vh" />
           </Box>
         </Flex>
       </Container>
