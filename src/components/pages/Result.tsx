b8523a653b737f7baf3c0fa1bb41a2325e6dff2c
Re-taro <aminokacho@gmail.com>
2021-12-13 01:00:33 +0900

    🎨 Add justify

--- src/components/pages/Result.tsx
+++ src/components/pages/Result.tsx
@@ -14,7 +14,7 @@ const Result: FC = () => {
     <ChakraProvider>
       <Container>
         <Flex flexDirection='column' alignItems='center'>
-          <Box align='center'>
+          <Box align='center' justify="center">
             <Text color="#02331b" align="center">登録が完了しました！</Text>
             <Text color="#5a7165" align="center">ブラウザを閉じてください。</Text>
             <Image alt="Thank you!" src={thankyou} w="50%" h="50%" />
