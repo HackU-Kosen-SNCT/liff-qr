f98a48ea4d92becc0136120f1cba797723e82558
Re-taro <aminokacho@gmail.com>
2021-12-13 20:58:03 +0900

    ✨ Change to use alert

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,21 @@
 import { render } from 'react-dom'
 import App from './components/pages/App'
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+const AlertTemplate = require('react-alert-template-basic')
+
+const options = {
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: '30px',
+  transition: transitions.SCALE
+}
 
 const rootElement = document.getElementById('root')
-render(<App />, rootElement)
+const Root = () => {
+  return (
+    <AlertProvider template={AlertTemplate} {...options}>
+      <App />
+    </AlertProvider>
+  )
+}
+render(<Root />, rootElement)
